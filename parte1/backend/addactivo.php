827b3cf0616af8aa76a42a6d4766d179937eaf2e
proreder <proreder@hotmail.com>
2022-04-13 23:50:28 +0200

    Ejercicio 6 Lista activos terminado

--- parte1/backend/addactivo.php
+++ parte1/backend/addactivo.php
@@ -1,12 +1,31 @@
 <?php
 var_dump($_POST);
 
- function checkCoherence(){
+//verificamos si se ha recibido el formulario
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    //filtramos los datos que han llegado via POST
+    $nombre=filter_input(INPUT_POST, 'nombre', FILTER_CALLBACK, ['options' => 'saneaCadena']);
+    $descripcion=filter_input(INPUT_POST, 'descripcion', FILTER_CALLBACK, ['options' => 'saneaCadena']);
+    $empresamnt=filter_input(INPUT_POST, 'empresamnt', FILTER_CALLBACK, ['options' => 'saneaCadena']);
+    $contactomnt=filter_input(INPUT_POST, 'contactomnt', FILTER_CALLBACK, ['options' => 'saneaCadena']);
+    $telefonomnt=filter_input(INPUT_POST, 'telefonomnt', FILTER_CALLBACK, ['options' => 'saneaCadena']);
+    $id=filter_input(INPUT_POST, 'nombre', FILTER_CALLBACK, ['options' => 'saneaCadena']);
+    if(is_numeric($id) && $id> 0){
+            $estatura=$_estatura;
+        }else{
+            $estatura=false;
+        }
+    //guardamos los datos llegados via post en el array asociativo
+    $data = ['nombre' => $nombre, 'descripcion' => $descripcion, 'empresamnt' => $empresamnt, 
+            'contactomnt' => $contactomnt, 'telefonomnt' => $telefonomnt];
+}
+
+ function checkCoherence($data){
         $array=[];
         //verifica si la longitud no supera los campos del la base de datos
         
-        $data = ['nombre' => $this->nombre, 'descripcion' => $this->descripcion, 'empresamnt' => $this->empresamnt, 
-                 'contactomnt' => $this->contactomnt, 'telefonomnt' => $this->telefonomnt];
+        //$data = ['nombre' => $this->nombre, 'descripcion' => $this->descripcion, 'empresamnt' => $this->empresamnt, 
+        //         'contactomnt' => $this->contactomnt, 'telefonomnt' => $this->telefonomnt];
         $rules = [
             'nombre' => ['required', 'minLen' => 2,'maxLen' => 45, 'nombre'],
             'descripcion' => ['maxLen' => 255, 'nombre'],
@@ -50,49 +69,51 @@ var_dump($_POST);
 
                     switch ($rule){
                         case 'required':
-                        if(empty($item_value) && $rule_value){
-                            $this->addError($item,ucwords($item). ' required');
-                        }
-                        break;
+                            if(empty($item_value) && $rule_value){
+                                $this->addError($item,ucwords($item). ' required');
+                            }
+                            break;
 
                         case 'minLen':
-                        if(strlen($item_value) < $rule_value){
-                            $this->addError($item, ucwords($item). ' tiene que ser mínimo de '.$rule_value. ' carácteres');
-                        }       
-                        break;
+                            if(strlen($item_value) < $rule_value){
+                                $this->addError($item, ucwords($item). ' tiene que ser mínimo de '.$rule_value. ' carácteres');
+                            }       
+                            break;
 
                         case 'maxLen':
-                        if(strlen($item_value) > $rule_value){
-                            $this->addError($item, ucwords($item). ' tiene que ser máximo de  '.$rule_value. ' carácteres');
-                        }
-                        break;
+                            if(strlen($item_value) > $rule_value){
+                                $this->addError($item, ucwords($item). ' tiene que ser máximo de  '.$rule_value. ' carácteres');
+                            }
+                            break;
 
                         case 'numeric':
-                        if(!ctype_digit($item_value) && $rule_value){
-                            $this->addError($item, ucwords($item). ' tiene que ser numérico');
-                        }
-                        break;
-                        case 'alpha':
-                        if(!ctype_alpha($item_value) && $rule_value){
-                            $this->addError($item, ucwords($item). ' tiene que ser carácteres alfabéticos');
-                        }
+                            if(!ctype_digit($item_value) && $rule_value){
+                                $this->addError($item, ucwords($item). ' tiene que ser numérico');
+                            }
+                            break;
+                            
+                            case 'alpha':
+                            if(!ctype_alpha($item_value) && $rule_value){
+                                $this->addError($item, ucwords($item). ' tiene que ser carácteres alfabéticos');
+                            }
                         case 'telefono':
-                        $regex='/^((?:\d{3}[ -]\d{2}[ -]\d{2}[ -]\d{2})|(?:\d{3}[ -]\d{3}[ -]\d{3})|(?:\d{9}))(?:[;](?1))*$/';
-                        
-                        if(!preg_match($regex, $item_value) & $item_value!==""){
-                            $this->addError($item, ucwords($item). ' el teléfono no tiene el formato correcto');
-                        }
-                        break;
+                            $regex='/^((?:\d{3}[ -]\d{2}[ -]\d{2}[ -]\d{2})|(?:\d{3}[ -]\d{3}[ -]\d{3})|(?:\d{9}))(?:[;](?1))*$/';
+                            if(!preg_match($regex, $item_value) & $item_value!==""){
+                                $this->addError($item, ucwords($item). ' el teléfono no tiene el formato correcto');
+                            }
+                            break;
                         case 'nombre':
-                        $regex='/^([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\']+[\s])+([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\'])+[\s]?([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\'])?$/';
-                        if(preg_match($regex, $rule_value) >0 ){
-                            $this->addError($item, ucwords($item). ' el nombre no tiene el formato correcto');
-                        }
+                            $regex='/^([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\']+[\s])+([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\'])+[\s]?([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\'])?$/';
+                            if(preg_match($regex, $rule_value) >0 ){
+                                $this->addError($item, ucwords($item). ' el nombre no tiene el formato correcto');
+                            }
+                            break;
                         case 'empresa':
-                        $regex="/^(\w)+\s{1}(\w)+[.,]+(\w)+[.,]$/";
-                        if(preg_match($regex, $rule_value) >0 ){
-                            $this->addError($item, ucwords($item). ' el nombre de empresa no tiene el formato correcto');
-                        }    
+                            $regex="/^(\w)+\s{1}(\w)+[.,]+(\w)+[.,]$/";
+                            if(preg_match($regex, $rule_value) >0 ){
+                                $this->addError($item, ucwords($item). ' el nombre de empresa no tiene el formato correcto');
+                            }
+                            break;
                     }
                 }
             }
@@ -115,7 +136,24 @@ var_dump($_POST);
         if(empty($_errors)) return false;
         return $_errors;
     }
+    
+    /**
+ * 
+ * @param string $valor cadena de entrada para validar y sanear
+ * @return string si es una cadena se devuelve la cadena de entrada o se 
+ *                se devuelve false si la cadena de entrada no es string
+ */
+function saneaCadena($valor){
+    //elimina los espacios
+    $_valor=trim($valor);
+    //elimina los caracteres especiales y los combierte en entidads html
+    $_valor= htmlspecialchars($_valor);
+    //Eliminar las barras invertidas
+    $cadena= stripcslashes($_valor);
+    $esCadena= is_string($cadena) ? $cadena : false;
+    return $esCadena;
 }
+
 /*
 el código javascript enviará un formulario $_POST con los datos siguientes (similares, esto es un ejemplo):
 
